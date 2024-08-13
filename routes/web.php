4ae3a3966d1ffb549d5b491430dfeb3262b28926
palden0929 <lamapaldenyolmo@gmail.com>
2024-08-13 10:05:15 +0545

    first

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+# Basic Route
+Route::get('/', [AppController::class, 'index']);
+
+# Route with Parameter
+Route::get('/about/{paramname}', [AppController::class, 'about']);
+
+# Route with optional parameter
+Route::get('/services/{paramname?}', function(){
+    return "<h1>Hello from services!</h1>";
 });
