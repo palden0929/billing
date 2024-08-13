9cb0e78d96c2453490a35f2189995db7b8671884
palden0929 <lamapaldenyolmo@gmail.com>
2024-08-13 10:42:18 +0545

    passing data

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -6,11 +6,35 @@ use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
-    public function index(){
-        return view("welcome");
+    public function index()
+    {
+        $name = "Palden";
+        $phone = "9841567890";
+        $city = "Kathmandu";
+
+        // #Using compact
+        // return view("welcome", compact("name","phone","city"));
+
+        // #Using associative array
+        // return view("welcome",array(
+        //     'name1' => $name,
+        //     'phone' => $phone,
+        //     'city' => $city,        ));
+
+        // #Using with method
+        // return view("welcome")
+        // ->with('name', $name)
+        // ->with('phone', $phone)
+        // ->with('city', $city);
+
+        $data['name'] = "Palden";
+        $data["phone"] = "9841456789";
+        $data["city"] = "Kathmandu";
+        return view("welcome", $data);
     }
 
-    public function about(){
+    public function about()
+    {
         return view("about");
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,3 @@
-@php 
-            $name = "palden";
-    echo $name;
-
-@endphp
-
-@if(1 > 2)
-    <h1>Hello World</h1>
-@else
-    <h1>Hello Palden</h1>
-@endif
-
-@foreach ()
-    
-@endforeach
+<h1>Name: {{$name}}</h1>
+<h1>Phone: {{$phone}}</h1>
+<h1>Ciry: {{$city}}</h1>
