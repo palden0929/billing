881545fe627887b3fd81cbc4a2d80d5ad8bac963
palden0929 <lamapaldenyolmo@gmail.com>
2024-08-19 11:01:29 +0545

    Query Builder

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -4,50 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Party;
+use Illuminate\Support\Facades\DB;
 
 class AppController extends Controller
 {
     public function index()
     {
         // Insert
-        $party = new Party();
-        $party->full_name = "Palden Lama";
-        $party->phone_no = "9845554151";
-        //$party->save();
-
-        // Insert - option 2
         $param = array(
-            "full_name" => "Ajit",
-            "phone_no" => "9841098765",
-            "city" => "Sundarijal",
-            "address" => "Fata Tol",
+            "full_name" => "Palden",
         );
-        //Party::create($param);
+        //DB::table("parties")->insert($param);
 
-        // Select all records
-        $parties = Party::all();
-        //dd($parties);
+        // Select
+        $party = DB::table("parties")
+            -> get();
+        
+        // Select one specific record
+        $party = DB::table("parties")
+            ->where('phone_no',"9841098765")
+            -> get();
+        
+        echo "<pre>";
+        print_r($party);
 
-        // Select one record by id
-        // $id = 1;
-        //$party = Party::find($id);
-        //dd($party);
+        // Update
+        $param = array(
+            "full_name" => "Hariram Prasad",
+        );
+        // DB::table("parties")
+        // ->where("id",6)
+        // ->update($param);
 
-        // Select one record by another column/field
-        // $party = Party::where("phone_no","9841098765")->get();
-        // dd($party);
-
-        //Update
-        $id = 1;
-        $party = Party::find($id);
-        $party->full_name = "Suman Shrestha";
-        $party->phone_no = "9841567890";
-        //$party->save();
-
-        //Delete
-        $id = 3;
-        $party = Party::find($id);
-        $party->delete();
+        // Delete
+        DB::table("parties")
+        ->where("id",7)
+        ->delete();
 
         return "Database Operations";
         //return view("welcome");
